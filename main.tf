9667e83d5472ae69ef20c7eac7e934a0a9323b6e
Amandeep Singh <amandees@thoughtworks.com>
2019-01-30 15:11:00 +0530

    adding missing file

--- main.tf
+++ main.tf
@@ -7,5 +7,5 @@ module "ci" {
   source = "modules/concourseCI"
   public_subnet_id = "${module.vpc.public_subnet_id}"
   vpc_id = "${module.vpc.vpc_id}"
-  cidr_to_whitelist = "103.240.237.132/32"
+  cidr_to_whitelist = "${var.public_ip_to_whitelist}"
 }
