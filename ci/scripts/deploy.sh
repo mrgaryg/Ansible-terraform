85431038d516ebfdb843eee9bb22c1156dfcbc01
Amandeep Singh <amandees@thoughtworks.com>
2019-02-08 15:04:31 +0530

    ec2 ssh

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-ssh  -o StrictHostKeyChecking=no -i "${1}" ec2-user@172.20.20.75 'docker run amiedeep2/amiedeep:latest'
+ssh  -o StrictHostKeyChecking=no -i ssh_file ec2-user@172.20.20.75 'docker run amiedeep2/amiedeep:latest'
